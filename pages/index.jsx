0355c80e4013f9a0518bb4dcaa6205296fdd52c6
kawabata <kazuki.kawabata02@gmail.com>
2021-10-24 08:29:43 +0900

    完成

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,16 +5,14 @@ import Head from "next/head";
 import { Box, Heading } from "@chakra-ui/layout";
 
 export default function Home() {
-  const { currentUser } = useContext(AuthContext);
+  // const { currentUser } = useContext(AuthContext);
   return (
     <div>
       <Head>
         <title>Todoリスト</title>
       </Head>
       <Box bg="tomato" p="2">
-        <PageLink href={currentUser ? "/todos" : "/signin"} >
-          Todoページへ
-        </PageLink>
+        <PageLink href="/todos">Todoページへ</PageLink>
       </Box>
       <Heading as="h2" size="3xl" mt={5}>
         Topページ
