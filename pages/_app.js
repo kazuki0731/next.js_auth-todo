edb194f1647115704d85f488ea8af7b1ac5a8fe5
kawabata <kazuki.kawabata02@gmail.com>
2021-10-24 09:43:24 +0900

    スタイル以外完成

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import AuthProvider from "../components/authProvider";
-import TodosProvider from "../components/TodosProvider.jsx";
+import TodosProvider from "../components/TodosProvider";
 import { theme } from "../styles/theme";
 
 function MyApp({ Component, pageProps }) {
